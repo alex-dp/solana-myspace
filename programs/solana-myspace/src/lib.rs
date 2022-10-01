f6752fb8cac2c951112e57fbdee5e90d115ef507
Alessandro De Pasquale <depasquale.a@tuta.io>
2022-10-01 13:52:52 +0200

    initial

---
+++ programs/solana-myspace/src/lib.rs
@@ -0,0 +1,26 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod solana_myspace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+#[account]
+pub struct PersonalAccount {
+    most_recent_post: Post
+}
+
+
+#[account]
+pub struct Post {
+    text: String
+}
